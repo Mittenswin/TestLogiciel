d9136d5275c84413fcbf6cb6555fd00058ef21f9
MittensWin <oneskyfire@gmail.com>
2025-05-21 10:57:45 +0200

    StatistiqueTest

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -1,32 +1,29 @@
 package com.example.demo.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.example.demo.data.Voiture;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-
 
 @SpringBootTest
 public class StatistiqueTests {
 
-    @MockBean
-    StatistiqueImpl statistiqueImpl;
+    @Test
+    void statistiqueVoiture() {
+        StatistiqueImpl statistique = new StatistiqueImpl();
 
+        Voiture ferrari = mock(Voiture.class);
+        when(ferrari.getPrix()).thenReturn(1000);
 
+        statistique.ajouter(ferrari);
 
-    public void avecMockito() throws Exception{
+        Echantillon echantillon = statistique.prixMoyen();
 
-        StatistiqueImpl uneStats = new StatistiqueImpl();
-        Voiture uneVoiture = mock(Voiture.class);
-
-
-        when(uneVoiture.getPrix()).thenReturn(1000);
-        uneStats.ajouter(uneVoiture);
-        Echantillon e = uneStats.prixMoyen();
-        assertEquals(e.getPrixMoyen(),1000);
+        assertEquals(echantillon.getNombreDeVoitures(), 1);
+        assertEquals(echantillon.getPrixMoyen(), 1000);
     }
 
 }
